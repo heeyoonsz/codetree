6f94ca8b99cf278fb25a2c61ec1cb106a9a341a0
yoon <62053162+heeyoonsz@users.noreply.github.com>
2025-06-12 16:13:19 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250612/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int num;
+    cin >> num;
+    cout << num << endl;
+    if (num<0) {
+        cout << "minus";
+    }
+    return 0;
+}
