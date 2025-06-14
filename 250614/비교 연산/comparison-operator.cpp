a8dadf0daa4beb77cb77842ac54464c302209436
yoon <62053162+heeyoonsz@users.noreply.github.com>
2025-06-14 17:07:43 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250614/비교 연산/comparison-operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    cout << ((a>=b) == true? 1:0)<< endl;
+    cout << ((a>b) == true? 1:0)<< endl;
+    cout << ((a<=b) == true? 1:0)<< endl;
+    cout << ((a<b) == true? 1:0)<< endl;
+    cout << ((a==b) == true? 1:0)<< endl;
+    cout << ((a!=b) == true? 1:0) << endl;    
+            
+                
+                    return 0;
+}
