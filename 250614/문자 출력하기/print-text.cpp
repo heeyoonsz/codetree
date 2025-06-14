d8f471a92569f1213b25e30aea30329106c4d28b
yoon <62053162+heeyoonsz@users.noreply.github.com>
2025-06-14 20:46:35 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250614/문자 출력하기/print-text.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+    for(int i=0;i<8;i++) {
+        cout << str;
+    }
+    return 0;
+}
