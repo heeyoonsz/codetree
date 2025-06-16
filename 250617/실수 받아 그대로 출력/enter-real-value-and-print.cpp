662c135a9bbf5890d588c6da151ad1024d07f978
yoon <62053162+heeyoonsz@users.noreply.github.com>
2025-06-17 08:46:07 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250617/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double n;
+    cin >> n;
+    cout << round(n*100)/100;
+    return 0;
+}
