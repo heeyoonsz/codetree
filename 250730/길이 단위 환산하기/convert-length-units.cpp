18b269c2bd6e5e2398ea56248c70823ad42d38ce
yoon <62053162+heeyoonsz@users.noreply.github.com>
2025-07-30 09:39:54 +0900

    Title: 길이 단위 환산하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250730/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double n;
+    cin >> n;
+    cout << round(n*30.48*10)/10;
+    return 0;
+}
