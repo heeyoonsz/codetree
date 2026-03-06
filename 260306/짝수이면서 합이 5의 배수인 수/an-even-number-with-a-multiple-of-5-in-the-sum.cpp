bc1998f7c0d09d1782db8333330ca2cff7087125
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-03-06 16:05:26 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260306/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int yesNo(int n){
+    int sum = 0;
+    while(n>0){
+        sum+=n%10;
+        num/=10;
+    }
+    if(n%2==0 && num%5 ==0)
+    cout << "Yes" <<endl;
+    else
+    cout << "No" << endl;
+}
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    yesNo(n);
+    return 0;
+}
