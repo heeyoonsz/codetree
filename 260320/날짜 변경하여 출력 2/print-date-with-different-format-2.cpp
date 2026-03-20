0cb1b2093da3c4e3d103c28e511eccba1493e81d
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-03-20 11:25:05 +0900

    Title: 날짜 변경하여 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260320/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // Please write your code here.
+    int month,day,year;
+    char a,b;
+
+    cin >> month >> a >> day >> b >> year;
+    a='.';
+    cout << year << a << month << a << day;
+
+
+    return 0;
+}
