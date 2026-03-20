7a0481ed2ca4d067adce965aa9c7683aa230f5b6
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-03-20 13:38:49 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260320/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a, x, y;
+    char d1, d2;
+
+    cin >> a >> d1 >> x >> d2 >> y;
+    cout << a << d1 << y << d2 << x;
+    
+    return 0;
+}
