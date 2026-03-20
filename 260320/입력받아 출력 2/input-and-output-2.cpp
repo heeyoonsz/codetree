b2db1af054fc1300941204f44fb444c1a2c9f864
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-03-20 11:29:24 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260320/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int front,back;
+    char a;
+    cin >> front >> a >> back;
+    cout <<front<<back;
+    return 0;
+}
