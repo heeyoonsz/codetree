02cf3b219dba5f8b26d199a18a99337d6af7dbda
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-03-28 02:31:48 +0900

    Title: 오름 내림차순 정렬, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260328/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define MAX_N 100
+
+int n;
+int nums[MAX_N];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    // Please write your code here.
+    sort(nums,nums+n);
+    for(int i=0;i<n;i++){
+        cout << nums[i]<<" ";
+    }
+    cout << "\n";
+    sort(nums,nums+8,greater<int>());
+    for(int i=0;i<n;i++){
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
