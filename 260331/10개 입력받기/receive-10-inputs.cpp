e827b49977c3035824748063a4ff5c2fbae69004
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-03-31 15:41:01 +0900

    Title: 10개 입력받기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260331/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    int cnt = 0;
+    int sum = 0;
+    int arr[10];
+
+    for(int i=0;i<10;i++) {
+        cin >> arr[i];
+        sum += arr[i];
+        if(arr[i]==0){
+            cnt--;
+            cout << fixed << setprecision(1) << sum << " " << double(sum/i);
+        }
+    }
+
+
+
+
+
+    return 0;
+}
