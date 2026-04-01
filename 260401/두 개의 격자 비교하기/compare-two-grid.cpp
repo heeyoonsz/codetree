3ab17defc08222f8cdfa10f1249ae06985d8624c
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-04-01 11:42:29 +0900

    Title: 두 개의 격자 비교하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260401/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,m;
+    int arr[10][10], arr2[10][10];
+    cin >> n >> m;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> arr2[i][j];
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if (arr[i][j]==arr2[i][j]){
+                cout << 0 << " ";
+            }
+            else
+                cout << 1 << " ";
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
+}
