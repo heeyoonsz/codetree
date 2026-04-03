ca78523786bf45a54133d126a438b6ce271e86cb
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-04-03 11:06:16 +0900

    Title: 단어로 구분하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    
+    string str;
+
+    for(int i=0;i<10;i++){
+        cin >> str;
+        if(i%2==0) {
+            cout << str << "\n";
+        }
+    }
+    
+    return 0;
+}
