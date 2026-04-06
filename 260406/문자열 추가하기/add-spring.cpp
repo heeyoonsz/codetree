a9c864ec05359456f42b899a2efc271eeac9e4bd
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-04-06 10:25:35 +0900

    Title: 문자열 추가하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260406/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string str;
+    string greeting = "Hello";
+    cin >> str;
+    cout << str+greeting;
+    
+    return 0;
+}
