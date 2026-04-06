f42ae04fbd55137cf427c4e38d3033e9b38fd272
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-04-06 11:59:15 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260406/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a,b;
+    cin >> a >> b;
+    if(a+b==b+a){
+        cout <<"true";
+    }
+    else
+        cout <<"false";
+    return 0;
+}
