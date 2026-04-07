83225696cc3bcdee8eb2a8778652a58149786981
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-04-07 14:37:57 +0900

    Title: 문자 교체하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/문자 교체하기/changing-char.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a,b;
+    cin >> a >> b;
+    
+    // string part = a[0]+a[1];
+    // string b_substr= b.erase(0,1);
+
+    b[0] = a[0];
+    b[1] = a[1];
+
+    cout << b;
+
+    return 0;
+}
