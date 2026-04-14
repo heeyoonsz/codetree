5d14a4dc5c285e33ea206d8e54d900d1e4bfaa62
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-04-14 10:55:16 +0900

    Title: 그 다음 알파벳, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260414/그 다음 알파벳/next-alphabet.cpp
+++ 260414/그 다음 알파벳/next-alphabet.cpp
@@ -5,6 +5,11 @@ int main() {
     // Please write your code here.
     char a;
     cin >> a;
+    if(a=='z'){
+        cout << 'a';
+    }
+    else {
     cout << (char)((int)a + 1) ;
+    }
     return 0;
 }
