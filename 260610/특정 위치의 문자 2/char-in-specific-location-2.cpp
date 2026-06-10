84d28067853253bc76c3e5cdd09d624bddf53ad1
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-06-10 09:41:05 +0900

    [Passed] 특정 위치의 문자 2 (2ms, 0MB)

---
+++ 260610/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string arr[10];
+
+    for(int i=0;i<10;i++){
+        cin >> arr[i];
+    }
+
+    cout << arr[1] << " " << arr[4] << " " << arr[7] << endl;
+
+    return 0;
+}
