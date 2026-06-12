14e22c15983ff2f0f79dc16706c9f239f35dc578
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-06-12 10:48:44 +0900

    [Passed] 전항의 두 배 (2ms, 0MB)

---
+++ 260612/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[10];
+    int a,b;
+    cin >> a >> b;
+    arr[0] = a;
+    arr[1] = b;
+    for(int i=2;i<10;i++) {
+        arr[i] = arr[i-1] + 2*arr[i-2];
+    }   
+    for(int i=0;i<10;i++){
+        cout << arr[i] << " ";
+    } 
+
+    return 0;
+}
