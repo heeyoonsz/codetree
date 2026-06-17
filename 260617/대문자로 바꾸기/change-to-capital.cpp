627735816f89c2cd7cefc07b58e3b0e9a36711c3
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-06-17 22:27:43 +0900

    [Passed] 대문자로 바꾸기 (2ms, 0MB)

---
+++ 260617/대문자로 바꾸기/change-to-capital.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    char arr_2d[5][3];
+
+    for(int i=0;i<5;i++){
+        for(int j = 0;j<3;j++){
+            cin >> arr_2d[i][j];
+        }
+    }
+
+    for(int i=0;i<5;i++){
+    for(int j = 0;j<3;j++){
+        cout << (char)(toupper(arr_2d[i][j])) <<" ";
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
+}
