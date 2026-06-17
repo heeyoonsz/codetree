802989f5c6598808ab07af7a48e755005cf6f312
yoon <62053162+heeyoonsz@users.noreply.github.com>
2026-06-17 22:21:04 +0900

    [Passed] 배열의 합 (2ms, 0MB)

---
+++ 260617/배열의 합/sum-of-array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr_2d[4][4];
+    for(int i=0;i<4;i++){
+    int sum = 0;
+        for(int j=0;j<4;j++){
+            cin >> arr_2d[i][j];
+            sum += arr_2d[i][j];
+        }
+    cout << sum<<endl;
+    }
+
+
+    return 0;
+}
